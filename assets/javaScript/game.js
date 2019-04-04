9bd2822e117526055b3fb9cdb420c59294657ddc
shrudder <seanmrudd@me.com>
2019-04-03 22:47:12 -0400

    not even halfway

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,65 +1,78 @@
+$(document).ready(function() {
+
 var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 $('#random-number').text(randomNumber)
 
+// var randomRed = Math.floor(Math.random() * (12)) + 1;
+// console.log(randomRed)
+// $('#ruby').val(randomRed)
+
+// var randomBlue = Math.floor(Math.random() * (12)) + 1;
+// console.log(randomBlue)
+// $('#sapphire').val(randomBlue)
 
 
-var randomRed = Math.floor(Math.random() * (12)) + 1;
-console.log(randomRed)
-$('#ruby').val(randomRed)
-
-var randomBlue = Math.floor(Math.random() * (12)) + 1;
-console.log(randomBlue)
-$('#sapphire').val(randomBlue)
+// var randomYellow = Math.floor(Math.random() * (12)) + 1;
+// console.log(randomYellow)
+// $('#amber').val(randomYellow)
 
 
-var randomYellow = Math.floor(Math.random() * (12)) + 1;
-console.log(randomYellow)
-$('#amber').val(randomYellow)
+// var randomGreen = Math.floor(Math.random() * (12)) + 1;
+// console.log(randomGreen)
+// $('#emerald').val(randomGreen)
 
+var randomCrystal = Math.floor(Math.random() * (12)) + 1;
+$('.crystal').val(randomCrystal)
 
-var randomGreen = Math.floor(Math.random() * (12)) + 1;
-console.log(randomGreen)
-$('#emerald').val(randomGreen)
+var totalScore = 0;
+var gameScore = totalScore;
+var win = 0;
+var loss = 0;
 
-// var randomCrystal = Math.floor(Math.random() * (12)) + 1;
-// $('.crystal').val(randomCrystal)
-
-totalScore = 0;
-$('#total-score').text(totalScore)
-
-$('#ruby').on('click', function () {
-    crystalValue = randomRed;
+$('.crystal').on('click', function () {
+    crystalValue = randomCrystal;
     totalScore = totalScore + crystalValue;
     console.log(crystalValue);
     console.log(totalScore);
+    $('#total-score').text(gameScore)
 })
 
-$('#sapphire').on('click', function () {
-    crystalValue = randomBlue;
-    totalScore = totalScore + crystalValue;
-    console.log(crystalValue);
-    console.log(totalScore);
-})
+// $('#ruby').on('click', function () {
+//     crystalValue = randomRed;
+//     totalScore = totalScore + crystalValue;
+//     console.log(crystalValue);
+//     console.log(totalScore);
+//     $('#total-score').text(gameScore)
+// })
 
-$('#amber').on('click', function () {
-    crystalValue = randomYellow;
-    totalScore = totalScore + crystalValue;
-    console.log(crystalValue);
-    console.log(totalScore);
-})
+// $('#sapphire').on('click', function () {
+//     crystalValue = randomBlue;
+//     totalScore = totalScore + crystalValue;
+//     console.log(crystalValue);
+//     console.log(totalScore);
+// })
 
-$('#emerald').on('click', function () {
-    crystalValue = randomGreen;
-    totalScore = totalScore + crystalValue;
-    console.log(crystalValue);
-    console.log(totalScore);
-})
+// $('#amber').on('click', function () {
+//     crystalValue = randomYellow;
+//     totalScore = totalScore + crystalValue;
+//     console.log(crystalValue);
+//     console.log(totalScore);
+// })
 
-if (totalScore === randomNumber) {
+// $('#emerald').on('click', function () {
+//     crystalValue = randomGreen;
+//     totalScore = totalScore + crystalValue;
+//     console.log(crystalValue);
+//     console.log(totalScore);
+// })
+
+if (gameScore === randomNumber) {
     win++;
-} else if (totalScore > randomNumber) {
+} else if (gameScore > randomNumber) {
     loss++;
 }
 
+
 $('#win').html(win)
 $('#loss').html(loss)
+})
