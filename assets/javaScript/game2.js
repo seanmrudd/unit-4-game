782978cca80d8ed605d865270f74cfba0d613990
Sean Rudd <seanmrudd@me.com>
2019-04-05 21:10:30 -0400

    almost there

--- assets/javaScript/game2.js
+++ assets/javaScript/game2.js
@@ -1,62 +1,83 @@
-var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-$('#random-number').text(randomNumber)
-
-
-w = Math.floor(Math.random() * (12)) + 1;
-x = Math.floor(Math.random() * (12)) + 1;
-y = Math.floor(Math.random() * (12)) + 1;
-z = Math.floor(Math.random() * (12)) + 1;
-var numberOptions = [w, x, y, z];
-console.log(numberOptions)
-
-
-for (var i = 0; i < numberOptions.length; i++) {
-
-  var imageCrystal = $("<img>");
-
-
-  imageCrystal.addClass("crystal-image");
-
-
-  imageCrystal.attr("src", "assets/images/ruby.jpg");
-
-
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-  $('.crystals-box').append(imageCrystal);
-}
 
 var totalScore = 0;
 var win = 0;
 var loss = 0;
 
 
-$(".crystal-image").on("click", function () {
+$(document).ready(function () {
+
+  var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  $('#random-number').text(randomNumber)
 
 
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
+  var w = Math.floor(Math.random() * (12)) + 1;
+  var x = Math.floor(Math.random() * (12)) + 1;
+  var y = Math.floor(Math.random() * (12)) + 1;
+  var z = Math.floor(Math.random() * (12)) + 1;
+  var numberOptions = [w, x, y, z];
+  console.log(numberOptions)
 
-  totalScore += crystalValue;
 
+  var images = ['assets/images/ruby.jpg', 'assets/images/sapphire.jpg', 'assets/images/amber.jpg', 'assets/images/emerald.jpg']
 
-  if (totalScore === randomNumber) {
-    win++;
-    restart();
-    console.log(win)
-  } else if (totalScore >= randomNumber) {
-    ++loss;
-    restart();
-    console.log(loss)
+  for (var i = 0; i < images.length; i++) {
+
+    imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", images[i]);
+
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    $('.crystals-box').append(imageCrystal);
+
   }
-  
+
+
+
   function restart() {
-    var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $('#random-number').text(randomNumber)
+
     totalScore = 0;
+
+    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $('#random-number').text(randomNumber)
+    console.log(randomNumber);
+
+    w = Math.floor(Math.random() * (12)) + 1;
+    x = Math.floor(Math.random() * (12)) + 1;
+    y = Math.floor(Math.random() * (12)) + 1;
+    z = Math.floor(Math.random() * (12)) + 1;
+    numberOptions = [w, x, y, z];
+    console.log(numberOptions);
+
+    for (var i = 0; i < images.length; i++) {
+      imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+      console.log(numberOptions[i])
+    }
   }
 
-  $('#total-score').text(totalScore)
-  $('#win').text(win)
-  $('#loss').text(loss)
-});
+
+  $(".crystal-image").on("click", function () {
+
+    crystalValue = ($(this).attr("data-crystalvalue"));
+    console.log(this)
+    crystalValue = parseInt(crystalValue);
+
+    totalScore += crystalValue;
+
+    if (totalScore === randomNumber) {
+      win++;
+      restart();
+      console.log('win: ' + win)
+    } else if (totalScore >= randomNumber) {
+      ++loss;
+      restart();
+      console.log('loss: ' + loss)
+    }
+
+    $('#total-score').text(totalScore)
+    $('#win').text(win)
+    $('#loss').text(loss)
+  });
+})
